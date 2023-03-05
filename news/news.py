fb3a3718905994458a86cb5575898133ac5b88cc
Rafael Lopes <rafaelslopes159@gmail.com>
2023-03-05 02:35:50 -0300

    feat: news, content and gpt bots started

--- news/news.py
+++ news/news.py
@@ -1,25 +1,26 @@
 from news.headlines import Headlines
 from news.content import NewsContent
 from newspaper import ArticleException
+from typing import List
 
 
 class News:
-  def get_news(self, country='BR', language='pt-BR', num_news=5):
-    self.num_news = num_news
-    headlines = Headlines(country, language, num_news*2)
-    headlines = headlines.get_headlines()
-    news = self.__get_structured_news(headlines)
-    return news 
+    def get_news(self, country='BR', language='pt-BR', num_news=5) -> List[NewsContent]:
+        self.num_news = num_news
+        headlines = Headlines(country, language, num_news*2)
+        headlines = headlines.get_headlines()
+        news = self.__get_structured_news(headlines)
+        return news
 
-  def __get_structured_news(self, headlines):
-    articles = []
-    i = 0
-    while i < len(headlines) and len(articles) < self.num_news:
-        headline = headlines[i]
-        i += 1
-        try:
-            articles.append(NewsContent(headline))
-        except ArticleException:
-            continue
-          
-    return articles
+    def __get_structured_news(self, headlines) -> List[NewsContent]:
+        articles = []
+        i = 0
+        while i < len(headlines) and len(articles) < self.num_news:
+            headline = headlines[i]
+            i += 1
+            try:
+                articles.append(NewsContent(headline))
+            except ArticleException:
+                continue
+
+        return articles
